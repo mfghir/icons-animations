0709b38f498875c2e3995f9a488f7ce12f19d9b6
miss-f-g <fatemeghafari98@gmail.com>
2022-07-26 10:17:12 +0430

    6.add anim to ringing phone

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const tl = gsap.timeline({
 
 const home = document.querySelector(".home");
 const notifications = document.querySelector(".notifications");
+const messages = document.querySelector(".messages");
+const ringing = document.querySelector(".ringing");
 
-
-// home 🏠 
+// home 🏠
 gsap.set(".feather", { scale: 0, transformOrigin: "center" });
 home.addEventListener("click", () => {
   gsap.fromTo(".home-svg", { scale: 1 }, { scale: 0.9, yoyo: true, repeat: 1 });
@@ -17,7 +18,6 @@ home.addEventListener("click", () => {
   );
   gsap.fromTo(".right-feather", { x: 0 }, { x: 5 });
 });
-
 
 // bell 🔔
 gsap.set(".bell", { transformOrigin: "top center" });
@@ -42,13 +42,10 @@ notifications.addEventListener("click", () => {
   );
 });
 
-
-
 //Messages 💌
 gsap.set(".flap", { transformOrigin: "top" });
 
 messages.addEventListener("click", () => {
-
   tl.fromTo(".messages-svg", { scale: 1 }, { scale: 0.9 });
   tl.fromTo(".flap", { scale: 1 }, { scale: -1 }, "<50%");
   tl.fromTo(".messages-svg", { scale: 0.9 }, { scale: 1 }, "<50%");
@@ -59,5 +56,28 @@ messages.addEventListener("click", () => {
     { y: -40, opacity: 0, duration: 0.75 }
   );
   tl.to(".flap", { scale: 1 }, "<50%");
-  
+});
+
+// Ringing phone 📞
+gsap.set(".long-wave", { opacity: 0, transformOrigin: "bottom left" });
+gsap.set(".short-wave", { opacity: 0, transformOrigin: "bottom left" });
+gsap.set(".phone", {  transformOrigin: "center" });
+
+ringing.addEventListener("click", () => {
+  gsap.fromTo(
+    ".long-wave",
+    { scale: 0, opacity: 1 },
+    { scale: 1.3, opacity: 0, duration: 1 }
+  );
+  gsap.fromTo(
+    ".short-wave",
+    { scale: 0, opacity: 1 },
+    { scale: 1.3, opacity: 0, duration: 1 },
+    "<20%"
+  );
+  gsap.fromTo(
+    ".phone",
+    { rotation: -10 },
+    { rotation: 0, duration: 2, ease: "elastic.out(5, 0.2)" }
+  );
 });
