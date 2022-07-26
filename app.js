6450fc82ce86c8596008f103584685a7daf0f205
miss-f-g <fatemeghafari98@gmail.com>
2022-07-26 09:08:21 +0430

    4.add anim to bell svg

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const tl = gsap.timeline({
   defaults: { duration: 0.35, ease: "Power2.easeOut" },
 });
+
 const home = document.querySelector(".home");
+const notifications = document.querySelector(".notifications");
+
+
+// home 🏠 
 gsap.set(".feather", { scale: 0, transformOrigin: "center" });
-
-
 home.addEventListener("click", () => {
   gsap.fromTo(".home-svg", { scale: 1 }, { scale: 0.9, yoyo: true, repeat: 1 });
   gsap.fromTo(
@@ -13,4 +16,28 @@ home.addEventListener("click", () => {
     { y: 20, scale: 1.5, duration: 1, stagger: 0.2 }
   );
   gsap.fromTo(".right-feather", { x: 0 }, { x: 5 });
+});
+
+
+// bell 🔔
+gsap.set(".bell", { transformOrigin: "top center" });
+gsap.set(".ringer", { transformOrigin: "top center" });
+gsap.set(".wave", { opacity: 0, transformOrigin: "bottom" });
+
+notifications.addEventListener("click", () => {
+  gsap.fromTo(
+    ".bell",
+    { rotation: -5 },
+    { rotation: 0, duration: 2, ease: "elastic.out(5, 0.2)" }
+  );
+  gsap.fromTo(
+    ".ringer",
+    { rotation: -3, x: 0.5 },
+    { rotation: 0, x: 0, duration: 1, ease: "elastic.out(5, 0.2)" }
+  );
+  gsap.fromTo(
+    ".wave",
+    { scale: 0, opacity: 1 },
+    { scale: 1.3, opacity: 0, duration: 1 }
+  );
 });
